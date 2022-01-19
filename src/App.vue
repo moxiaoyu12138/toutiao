1627ad859601e22bd178636e56eb17f00cffd501
moxiaoyu12138 <1620889647@qq.com>
2022-01-19 17:06:58 +0800

    引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
   <div id="app">
     <router-view/>
     <h1>头条</h1>
+    <i class="toutiao toutiao-pinglun"></i>
+    <van-button type="primary">主要按钮</van-button>
+    <van-image
+      width="100"
+      height="100"
+      src="https://img01.yzcdn.cn/vant/cat.jpeg"
+    />
   </div>
 </template>
 
